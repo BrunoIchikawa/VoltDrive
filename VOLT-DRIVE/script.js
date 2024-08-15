0f664ebbe526a38297a6b3dd0266076cf3219703
Bruno Santos Ichikawa <149208859+BrunoIchikawa@users.noreply.github.com>
2024-08-15 16:20:29 -0300

    Delete VOLT-DRIVE/script.js

--- VOLT-DRIVE/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  var menuContent = document.getElementById('menu-content');
-  if (menuContent.style.display === 'block') {
-      menuContent.style.display = 'none';
-  } else {
-      menuContent.style.display = 'block';
-  }
-}
